a8cb52c5322cb815628e92009da2def38c2ff16d
ruben miguel corcoba <ruben.feina@gmail.com>
2019-05-22 23:14:13 +0200

    add activity

---
+++ app/src/main/java/com/adictosalainformatica/cleanrichnotes/features/add/EditNote.java
@@ -0,0 +1,16 @@
+package com.adictosalainformatica.cleanrichnotes.features.add;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.adictosalainformatica.cleanrichnotes.R;
+
+public class EditNote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_note);
+    }
+}

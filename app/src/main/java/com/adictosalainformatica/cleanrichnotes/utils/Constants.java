1482aba90e0566ae51b1eee0e2dc4251cd48fd84
ruben miguel corcoba <ruben.feina@gmail.com>
2019-05-25 04:02:28 +0200

    move strings to constants

---
+++ app/src/main/java/com/adictosalainformatica/cleanrichnotes/utils/Constants.java
@@ -0,0 +1,19 @@
+package com.adictosalainformatica.cleanrichnotes.utils;
+
+public class Constants {
+    public static final String ADD_NOTE = "ADD_NOTE";
+    public static final String EDIT_NOTE = "EDIT_NOTE";
+    public static final String SHOW_NOTE = "SHOW_NOTE";
+
+    public static final int RQ_ADD_NOTE = 20;
+
+    public static final String SCREEN_TYPE = "SCREEN_TYPE";
+    public static final String NOTE_MODEL = "NOTE_MODEL";
+
+    public static final String API_KEY = "yourKey";
+    public static final String CX = "yourKey";
+
+    public static final String DELETE_NOTE_KEY = "DELETE_NOTE_KEY";
+    public static final String PHOTO_PATH_KEY = "PHOTO_PATH_KEY";
+    public static final String PHOTO_PATH_DOWNLOADED_KEY = "PHOTO_PATH_DOWNLOADED_KEY";
+}
